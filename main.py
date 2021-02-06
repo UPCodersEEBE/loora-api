940046515ff2d16c14520e74ce87338b06d45456
a96lex <a96lex@gmail.com>
2021-02-06 14:29:51 +0100

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 from functions.time_functions import check_time
+from functions.database import store_ping
 
 
 app = Flask(__name__)
@@ -14,6 +15,7 @@ def ping():
     try:
         time = json_data["time"]
         response = check_time(time)
+        store_ping(json_data)
         return response
     except:
         return "We need time parameter"
