1f949fa1f9a3504553f790e54cbc3a3c674c4b4a
unknown <bilal.el.gham@estudiantat.upc.edu>
2021-02-06 14:20:56 +0100

    funcio comprovar temps

--- functions/time_functions.py
+++ functions/time_functions.py
@@ -1,4 +1,25 @@
-how to compare times in python 
+# how to compare times in python 
 
 
-def check_time(time):
+import datetime
+
+
+timeinput = "2022-01-01T00:00:00Z"
+clock = timeinput[:-1].split("T")[1]
+c = clock.split(":")
+
+
+date = timeinput.split("T")[0]
+d = date.split("-")
+
+
+now = datetime.datetime.now()
+#now = datetime.strftime(datetime.utcnow(),"%H:%M:%S") #output: 11:12:12
+timein = datetime.datetime(int(d[0]),int(d[1]), int(d[2]), int(c[0]), int(c[0]), int(c[0]))
+#timein = "10:12:34"
+
+if now >  timein:
+    print("Time has passed.")
+if now < timein:
+    print("Time has not passed yet")
+
