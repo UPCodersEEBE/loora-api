f6514dbfc6085a3e03077169ca8bce9d357157b2
a96lex <a96lex@gmail.com>
2021-02-06 12:41:06 +0100

    yaml deuna

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route("/ping", methods=["POST"])
+def search():
+    response = "Ping is added to db (not really but wouldn't it be nice)"
 
+    return response
 
 
 if __name__ == "__main__":
