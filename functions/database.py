771bf4d83d971dc0b69ba6d362b3ef35cbaa7ebd
a96lex <a96lex@gmail.com>
2021-02-06 14:27:27 +0100

    test db

--- functions/database.py
+++ functions/database.py
@@ -6,5 +6,4 @@ datastore_client = datastore.Client()
 def store_ping(ping):
     entity = datastore.Entity(key=datastore_client.key("visit"))
     entity.update(ping)
-
     datastore_client.put(entity)
