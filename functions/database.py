683faf6467c732b1405a644428ebd62c32d8adcb
a96lex <a96lex@gmail.com>
2021-02-06 16:41:59 +0100

    add function

--- functions/database.py
+++ functions/database.py
@@ -3,7 +3,15 @@ from google.cloud import datastore
 datastore_client = datastore.Client()
 
 
-def store_ping(ping):
+def store_alive(ping):
     entity = datastore.Entity(key=datastore_client.key("visit"))
     entity.update(ping)
     datastore_client.put(entity)
+
+
+def get_us(dev_id):
+    # print("helldo", dev_id)
+    # query = client.query(kind="visit")
+    # query.add_filter("dev_id", "=", dev_id)
+    # print("r")
+    return "2021-02-05T15:20:20Z"
