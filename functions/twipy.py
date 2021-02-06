0d88f52397a86dd880cb29749f8c451c3daa849a
a96lex <a96lex@gmail.com>
2021-02-06 16:42:23 +0100

    removed call

--- functions/twipy.py
+++ functions/twipy.py
@@ -4,21 +4,15 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 
-def send_whatsapp(recievernum, name):
-    message= f"Hey! It seems like {name} is moving around"
-    message = client.messages \
-                    .create(
-                        from_='whatsapp:+14155238886',
-                        body=message,
-                        to='whatsapp:+34'+recievernum
-                    )
 
-    print(message)
+def send_whatsapp(recievernum, name):
+    message = f"Hey! It seems like {name} is moving around"
+    message = client.messages.create(
+        from_="whatsapp:+14155238886", body=message, to="whatsapp:+34" + recievernum
+    )
 
     return
-
-send_whatsapp("695848183", "Clara")
