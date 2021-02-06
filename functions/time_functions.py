a3c2c9d5bf10fcaa6f4b214a0cd525ad86d94c7c
a96lex <a96lex@gmail.com>
2021-02-06 14:24:34 +0100

    to function

--- functions/time_functions.py
+++ functions/time_functions.py
@@ -1,25 +1,21 @@
-# how to compare times in python 
-
-
 import datetime
 
 
-timeinput = "2022-01-01T00:00:00Z"
-clock = timeinput[:-1].split("T")[1]
-c = clock.split(":")
+def time_check(timeinput):
+    clock = timeinput[:-1].split("T")[1]
+    c = clock.split(":")
 
+    date = timeinput.split("T")[0]
+    d = date.split("-")
 
-date = timeinput.split("T")[0]
-d = date.split("-")
+    now = datetime.datetime.now()
+    # now = datetime.strftime(datetime.utcnow(),"%H:%M:%S") #output: 11:12:12
+    timein = datetime.datetime(
+        int(d[0]), int(d[1]), int(d[2]), int(c[0]), int(c[0]), int(c[0])
+    )
+    # timein = "10:12:34"
 
-
-now = datetime.datetime.now()
-#now = datetime.strftime(datetime.utcnow(),"%H:%M:%S") #output: 11:12:12
-timein = datetime.datetime(int(d[0]),int(d[1]), int(d[2]), int(c[0]), int(c[0]), int(c[0]))
-#timein = "10:12:34"
-
-if now >  timein:
-    print("Time has passed.")
-if now < timein:
-    print("Time has not passed yet")
-
+    if now > timein:
+        return "Time has passed."
+    if now < timein:
+        return "Time has not passed yet"
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
+from functions.time_functions import check_time
+
 
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route("/ping", methods=["POST"])
-def search():
-    response = "Ping is added to db (not really but wouldn't it be nice)"
-
-    return response
+def ping():
+    json_data = request.get_json(force=True)
+    try:
+        time = json_data["time"]
+        response = check_time(time)
+        return response
+    except:
+        return "We need time parameter"
 
 
 if __name__ == "__main__":
